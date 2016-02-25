bfb9ce18ea99fe2ab7941c8d70cda2842dde34bd
Faux92 <alexjackhughes@gmail.com>
2016-02-25 14:53:31 +0000

    Initialize repository

---
+++ mysite/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+from polls import views
+
+urlpatterns = [
+    url(r'^polls/', include('polls.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.HomeView.as_view(), name='index'),
+]
